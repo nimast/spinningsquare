caab7ce9603303de1d491ffaa62e819bb15b3df9
nimast <nimast@users.noreply.github.com>
2020-07-19 20:14:30 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,15 +2,14 @@ var x = 0;
 
 function setup() { 
   createCanvas(400, 400);
-  rectMode(CENTER);
+  
 } 
 
 function draw() { 
-  background(220);
+  background(110);
 	
-  
-	x+= 0.02;
+  x+= 0.02;
   translate (width/2, height/2);
   rotate(x);
-	rect(0, 0, 100, 100);
+  rect(0, 0, 100, 100);
 }
